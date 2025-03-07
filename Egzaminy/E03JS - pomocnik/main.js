b116ec0a9d077c4b6acb62ed348f4c0ea7859c82
ScyzorykL <kipsztal@gmail.com>
2025-03-07 14:04:06 +0100

    commited hihihiha

--- Egzaminy/E03JS - pomocnik/main.js
+++ Egzaminy/E03JS - pomocnik/main.js
@@ -3,4 +3,10 @@ document.querySelector('button').addEventListener('click', function()
    let h = parseInt(document.querySelector('#h').value);
    let s = 100;
    const l = 50;
+
+   for(let i=1; i<=5; i++)
+   {
+      document.querySelector(`#td${i}`).style.backgroundColor = `hsl(${h}, ${s}%, ${l}%)`;
+      s -= 20;
+   }
 });
