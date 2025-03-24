6871b13f52018b5a3ef8a75ecd1f461f71c3b63a
ScyzorykL <kipsztal@gmail.com>
2025-03-24 13:21:06 +0100

    sigma

---
+++ Egzaminy/E04JS - usługi informatyczne zielony/main.js
@@ -0,0 +1,21 @@
+const wynik = document.querySelector("#wynik");
+const imie_ = document.querySelector("#imie");
+const nazwisko_ = document.querySelector("#nazwisko");
+const email_ = document.querySelector("#email");
+const usluga_ = document.querySelector("#usluga");
+
+function aktywacja()
+{
+    wynik.innerHTML = ``;
+
+    let imie = imie_.value;
+    let nazwisko = nazwisko_.value;
+    let email = email_.value.toLowerCase();
+    let usluga = usluga_.value;
+
+    wynik.innerHTML += imie + `` + nazwisko + `<br>`;
+    wynik.innerHTML += email + `<br>`;
+    wynik.innerHTML += `Usługa: ${usluga}`;
+
+    
+}
